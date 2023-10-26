ef6ce72dee54b3b170c4b95b21f43746a1371c18
Joseph Calungsod <joseph.calungsod@gmail.com>
2023-10-26 13:45:04 -0400

    added adminaccount variables

--- SocialMediaApp-backend/src/main/java/com/example/demo/entity/AdminAccount.java
+++ SocialMediaApp-backend/src/main/java/com/example/demo/entity/AdminAccount.java
@@ -1,4 +1,10 @@
 package com.example.demo.entity;
 
 public class AdminAccount {
+    int id;
+    String username;
+    String password;
+    String nameFirst;
+    String nameLast;
+    String role = "admin";
 }
