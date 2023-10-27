e7d8f234e1faefe16dce84422a57471aa49944ae
harriszk <zacharykeatonharris@gmail.com>
2023-10-27 13:46:16 -0400

    Comment service comments

--- SocialMediaApp-backend/src/main/java/com/example/demo/Service/CommentService.java
+++ SocialMediaApp-backend/src/main/java/com/example/demo/Service/CommentService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.example.demo.Entity.Account;
 import com.example.demo.Entity.Comment;
+import com.example.demo.Entity.Post;
 import com.example.demo.Repository.CommentRepository;
 
 @Component
@@ -18,51 +20,63 @@ public class CommentService {
     }
 
     /**
+     * Retrieves all of the comments that are currently in the database.
      * 
-     * @return
+     * @return The list of comments.
      */
     public List<Comment> getAllComments() {
         return null;
     }
 
     /**
+     * Retrieves all of the comments that are associated with a particular post.
      * 
-     * @return
+     * @param post The post from which the comments are associated with.
+     * @return The list of comments.
      */
-    public List<Comment> getAllCommentsByPost() {
+    public List<Comment> getAllCommentsByPost(Post post) {
         return null;        
     }
 
     /**
+     * Retrieves all of hte comments that are associated with a particular
+     * account.
      * 
-     * @return
+     * @param account The account from which the comments are associated with.
+     * @return The list of comments.
      */
-    public List<Comment> getAllCommentsByAccount() {
+    public List<Comment> getAllCommentsByAccount(Account account) {
         return null;
     }
 
     /**
+     * Adds a new comment to the database.
      * 
-     * @param comment
-     * @return
+     * @param comment The comment to be added.
+     * @return The comment that was added. Return null if it was unable to add
+     * the comment.
      */
     public Comment addComment(Comment comment) {
         return null;
     }
 
     /**
+     * Updated a comment that is currently in the database.
      * 
-     * @param comment
-     * @return
+     * @param comment The updated comment.
+     * @return The comment that was updated. Returns null if no such comment
+     * with the given id exists in the database.
      */
     public Comment updateComment(Comment comment) {
         return null;
     }
 
     /**
+     * Deletes a comment form the database.
      * 
-     * @param comment
-     * @return
+     * @param comment The comment to be deleted.
+     * @return The deleted comment. Returns null if no such comment with the
+     * given id exists in the database.
      */
     public Comment deleteComment(Comment comment) {
         return null;
