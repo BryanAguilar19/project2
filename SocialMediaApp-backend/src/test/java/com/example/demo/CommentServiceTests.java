4b235ef49268d8a34f65a74d904afdc507f42806
harriszk <zacharykeatonharris@gmail.com>
2023-10-27 14:01:41 -0400

    Added JUnit

--- SocialMediaApp-backend/src/test/java/com/example/demo/CommentServiceTests.java
+++ SocialMediaApp-backend/src/test/java/com/example/demo/CommentServiceTests.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.junit.Before;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class CommentServiceTests {
+    @Before
     public void setUp() {
         
     }
--- SocialMediaApp-backend/src/test/java/com/example/demo/PostServiceTests.java
+++ SocialMediaApp-backend/src/test/java/com/example/demo/PostServiceTests.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.junit.Before;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class PostServiceTests {
+    @Before
     public void setUp() {
         
     }
