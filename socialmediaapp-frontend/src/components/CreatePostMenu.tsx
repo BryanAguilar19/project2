55b7c49ec80372f1234deb76538fd435f3e739e9
harriszk <zacharykeatonharris@gmail.com>
2023-10-28 17:46:13 -0400

    Able to submit a new post

--- socialmediaapp-frontend/src/components/CreatePostMenu.tsx
+++ socialmediaapp-frontend/src/components/CreatePostMenu.tsx
@@ -1,7 +1,10 @@
-import { SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useContext, useState } from "react";
+import { AccountContext } from "../App";
+import { Post } from "../models/Post";
+import { postPostAPI } from "../service/PostService";
 
-export function CreatePostMenu(){
-
+export function CreatePostMenu() {
+    const accountContext = useContext(AccountContext);
     const [imageUrl, setImageUrlInput] = useState("");
     const [description, setDescriptionInput] = useState("");
     
@@ -9,18 +12,40 @@ export function CreatePostMenu(){
         let image = event.target as HTMLTextAreaElement;
             setImageUrlInput(image.value);       
     }
+
     function updateContentText(event:SyntheticEvent){
         let text = event.target as HTMLTextAreaElement;
             setDescriptionInput(text.value);
         
     }
 
+    function submitPost() {
+        console.log("Submitting a new post!")
+
+        const post: Post = {
+            account: accountContext.account,
+            imageUrl: imageUrl,
+            description: description,
+            likes: 0,
+            comments: []
+        }
+
+        postPostAPI(post)
+            .then(response => {
+                console.log(response);
+            })
+            .catch((response) => console.log("Some error occurred!" + response))
+    }
+
     return (
         <>
-        Image: <input value={imageUrl} onChange={updateImageUrl}></input>
-        Description: <input value={description} onChange={updateContentText}></input>
+            <label>Image URL:</label>
+            <input name = "imageUrl" value = {imageUrl} onChange = {updateImageUrl}></input>
+            
+            <label>Description:</label>
+            <input name = "description" value = {description} onChange = {updateContentText}></input>
         
+            <button onClick = {submitPost}>Submit</button>
         </>
-
     )
 }
--- socialmediaapp-frontend/src/models/Post.tsx
+++ socialmediaapp-frontend/src/models/Post.tsx
@@ -1,7 +1,7 @@
 import { Account } from "./Account";
 
 export interface Post {
-    id: number,
+    id?: number,
     account: Account,
     imageUrl: string,
     description: string,
