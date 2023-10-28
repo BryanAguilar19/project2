f08d1d9f988ad592ec7e1d3b410c04f289c1e39d
harriszk <zacharykeatonharris@gmail.com>
2023-10-28 15:20:19 -0400

    Login and register on backend

--- SocialMediaApp-backend/src/main/java/com/example/demo/Controller/AccountController.java
+++ SocialMediaApp-backend/src/main/java/com/example/demo/Controller/AccountController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.entity.Account;
 import com.example.demo.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,13 +21,46 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    /**
+     * Retrieves all of the accounts in the database.
+     * 
+     * @return A list of the accounts.
+     */
+    @GetMapping("account")
+    public List<Account> getAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    /**
+     * 
+     * @param account The account to be posted.
+     */
     @PostMapping("account")
-    public void postAccountAPI(@RequestBody Account account){
+    public void postAccount(@RequestBody Account account) {
         accountService.saveAccount(account);
     }
 
-    @GetMapping("account")
-    public List<Account> getAccountAPI(){
-        return accountService.getAllAccounts();
+    @PostMapping("/account/login")
+    public ResponseEntity<Account> login(@RequestBody Account submittedAccount) {
+        Account account = this.accountService.login(submittedAccount);
+        HttpStatus status = HttpStatus.OK;
+
+        if(account == null) {
+            status = HttpStatus.UNAUTHORIZED;
+        }
+
+        return new ResponseEntity<Account>(account, status);
+    }
+
+    @PostMapping("/account/register")
+    public ResponseEntity<Account> register(@RequestBody Account submittedAccount) {
+        Account account = this.accountService.register(submittedAccount);
+        HttpStatus status = HttpStatus.CREATED;
+
+        if(account == null) {
+            status = HttpStatus.BAD_REQUEST;
+        }
+
+        return new ResponseEntity<Account>(account, status);
     }
 }
